1d96a245643d9a46fc4f2b941fda58ea008e4496
pedro <pedro10toscano@gmail.com>
2025-09-24 00:34:47 -0300

    inclusao data

---
+++ src/data/usuariosData.ts
@@ -0,0 +1,5 @@
+export let usuarios = [
+  { id: 1, nome: "João Silva", email: "joao@email.com", papel: "usuario", idade: 30 },
+  { id: 2, nome: "Maria Souza", email: "maria@email.com", papel: "admin", idade: 40 },
+  { id: 3, nome: "Pedro Santos", email: "pedro@email.com", papel: "usuario", idade: 25 }
+]
