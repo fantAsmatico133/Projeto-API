44bcca7921a26818bbdf8f52d61d4c6b7ee94b3a
pedro <pedro10toscano@gmail.com>
2025-09-25 14:09:43 -0300

    server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import usuarioRouter from "./router/usuarioRouter"
+import publicacaoRouter from "./router/publicacaoRouter"
+
+const app = express()
+app.use(express.json())
+
+app.use("/usuario", usuarioRouter)
+app.use("/publicacao", publicacaoRouter)
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000")
+})
